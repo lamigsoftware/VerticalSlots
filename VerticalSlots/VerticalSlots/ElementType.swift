cbd43a9e782ce4b46047a2c7176b7159d7bd6953
Roy Rollan <royrollanappbiz@gmail.com>
2017-03-09 15:19:38 +1100

    Added structs and enums
    Added structs and enums

---
+++ VerticalSlots/VerticalSlots/ElementType.swift
@@ -0,0 +1,15 @@
+//
+//  ElementType.swift
+//  VerticalSlots
+//
+//  Created by Roy on 9/03/2017.
+//  Copyright © 2017 Lamig Software. All rights reserved.
+//
+
+import Foundation
+
+enum ElementType {
+    case normal
+    case scatter
+    //case substitute
+}
---
+++ VerticalSlots/VerticalSlots/WinSet.swift
@@ -0,0 +1,14 @@
+//
+//  WinSet.swift
+//  VerticalSlots
+//
+//  Created by Roy on 9/03/2017.
+//  Copyright © 2017 Lamig Software. All rights reserved.
+//
+
+import Foundation
+
+struct WinSet {
+    let element:Element
+    let layoutPositions:[LayoutPosition]
+}
