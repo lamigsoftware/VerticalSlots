de3cdf42e6695accee5f64a798d81605b2c3e0fe
Roy Rollan <royrollanappbiz@gmail.com>
2017-03-09 14:59:12 +1100

    Added Scene type enum
    added enum

---
+++ VerticalSlots/VerticalSlots/SceneType.swift
@@ -0,0 +1,14 @@
+//
+//  SceneType.swift
+//  VerticalSlots
+//
+//  Created by Roy on 9/03/2017.
+//  Copyright © 2017 Lamig Software. All rights reserved.
+//
+
+import Foundation
+
+enum SceneType {
+    case baseGame
+    case freeGames
+}
