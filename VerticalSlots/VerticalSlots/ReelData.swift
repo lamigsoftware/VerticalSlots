ead7a16f9a135811d1e62a2e5129d9f742f83b2c
Tushar Das <tushar.das@yahoo.com>
2017-03-10 11:06:56 +1100

    array elements
    populated 200 variables

---
+++ VerticalSlots/VerticalSlots/ReelData.swift
@@ -0,0 +1,11 @@
+//
+//  ReelData.swift
+//  VerticalSlots
+//
+//  Created by Tushar Das on 10/03/2017.
+//  Copyright © 2017 Lamig Software. All rights reserved.
+//
+
+import Foundation
+
+array rail1 = {“Cat", "Cat", "Cow", "Cat", "Panda", "Cat", "Panda", "Dog", "Cat", "Dog", "Dog", "Panda", "Cat", "Panda", "Dog", "Dog", "Cow", "Dog", "Cow", "Cow", "Cow", "Cat", "Cow", "Cat", "Cow", "Cow", "Cat", "Cow", "Cat", "Dog", "Panda", "Cow", "Cow", "Cat", "Cow", "Cow", "Panda", "Cow", "Cat", "Cat", "Dog", "Dog", "Dog", "Cat", "Cat", "Panda", "Cat", "Dog", "Panda", "Cat", "Cow", "Panda", "Cow", "Cat", "Cat", "Dog", "Dog", "Cow", "Cow", "Dog", "Cat", "Panda", "Cat", "Panda", "Cat", "Cat", "Panda", "Dog", "Dog", "Panda", "Cow", "Dog", "Cat", "Cat", "Dog", "Panda", "Dog", "Cat", "Cat", "Cow", "Cat", "Cat", "Panda", "Panda", "Cow", "Dog", "Cat", "Dog", "Cow", "Dog", "Panda", "Panda", "Cow", "Cow", "Dog", "Panda", "Cow", "Panda", "Cat", "Cow", "Dog", "Panda", "Panda", "Cat", "Dog", "Cow", "Cow", "Dog", "Dog", "Cow", "Cat", "Panda", "Cow", "Cat", "Cat", "Cow", "Cow", "Cow", "Cow", "Cat", "Cat", "Panda", "Panda", "Cow", "Cat", "Panda", "Cat", "Cat", "Panda", "Panda", "Dog", "Cow", "Panda", "Cat", "Cat", "Panda", "Cat", "Dog", "Cat", "Panda", "Dog", "Dog", "Cow", "Dog", "Cow", "Dog", "Dog", "Panda", "Panda", "Panda", "Cow", "Cow", "Cow", "Panda", "Cow", "Cat", "Panda", "Panda", "Cow", "Dog", "Dog", "Panda", "Panda", "Cat", "Dog", "Dog", "Cow", "Panda", "Dog", "Panda", "Panda", "Cat", "Cat", "Panda", "Dog", "Dog", "Cow", "Cow", "Dog", "Dog", "Dog", "Dog", "Dog", "Cow", "Dog", "Panda", "Dog", "Dog", "Cow", "Cat", "Dog", "Cow", "Dog", "Dog", "Panda", "Cow", "Cow", "Dog", "Panda", “Dog""d
